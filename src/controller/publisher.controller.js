685841831cf448c536a540197379aebf6b2ec47b
margolin <yurymargolin@inbox.ru>
2025-12-21 21:01:50 +0200

    Optimize publisher retrieval by author using raw SQL query with Sequelize for improved performance

--- src/controller/publisher.controller.js
+++ src/controller/publisher.controller.js
@@ -1,21 +1,21 @@
 import {Author, Book} from "../model/index.js";
+import {sequelize} from "../config/database.js";
+import {QueryTypes} from "sequelize";
 
 export const findPublishersByAuthor = async (req, res) => {
     const author = await Author.findByPk(req.params.name);
     if (!author) {
         return res.status(404).send({error: `Author with name ${req.params.name} not found`});
     }
-
-    const books = await Book.findAll({
-        include: {
-            model: Author, as: 'authors',
-            where: {name: req.params.name},
-            through: {attributes: []}
-        },
-        attributes: ['publisher'],
-        raw: true,
-        group: ['publisher']
-    })
-    const publishers = books.map(b => b.publisher);
-    return res.json(publishers);
+    const publishers = await sequelize.query(`
+                SELECT DISTINCT b.publisher
+                FROM books b
+                         JOIN books_authors ba ON b.isbn = ba.isbn
+                         JOIN authors a ON ba.author_name = a.name
+                WHERE a.name = :name
+                `, {
+        replacements: {name: req.params.name},
+            type: QueryTypes.SELECT
+        });
+    return res.json(publishers.map(p => p.publisher));
 }

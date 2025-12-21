ab2ac9ed5244fc90a015b9a31ffc6788cad222b8
margolin <yurymargolin@inbox.ru>
2025-12-21 20:44:50 +0200

    Refactor publisher retrieval by author to optimize query with Sequelize joins and grouping

--- src/controller/publisher.controller.js
+++ src/controller/publisher.controller.js
@@ -1,11 +1,21 @@
-import {Author} from "../model/index.js";
+import {Author, Book} from "../model/index.js";
 
 export const findPublishersByAuthor = async (req, res) => {
     const author = await Author.findByPk(req.params.name);
-    if(!author){
+    if (!author) {
         return res.status(404).send({error: `Author with name ${req.params.name} not found`});
     }
-    const books = await author.getBooks();
-    const publishers =[...new Set(books.map(b => b.publisher))];
+
+    const books = await Book.findAll({
+        include: {
+            model: Author, as: 'authors',
+            where: {name: req.params.name},
+            through: {attributes: []}
+        },
+        attributes: ['publisher'],
+        raw: true,
+        group: ['publisher']
+    })
+    const publishers = books.map(b => b.publisher);
     return res.json(publishers);
 }
